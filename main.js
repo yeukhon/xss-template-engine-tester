8347d95de8aeb42a7e64a9dcddf0d960156930d5
Yeuk Hon Wong <yeukhon@acm.org>
2014-07-23 00:08:01 +0000

    Switch from wd.js to selenium-webdriver.js (official webdriverJS)

--- main.js
+++ main.js
@@ -1,98 +1,74 @@
-var chai = require("chai"),
-    chaiAsPromised = require("chai-as-promised")
-    wd = require('wd'),
-    Q = require('q'),
-        require('colors');
+var webdriver = require('selenium-webdriver');
+var Q = require('q');
 
-chai.use(chaiAsPromised);
-chai.should();
+function browser(fpath) {
 
-// enables chai assertion chaining
-chaiAsPromised.transferPromiseness = wd.transferPromiseness;
-
-wd.addPromiseChainMethod(
-    'checkAlert',
-    function () {
-        var deferred = Q.defer();
-        var result = {
-            "type": "alert",
-            "seen": false,
-            "text": ""
-        }
-//        try {
-            this.alertText(function (cb, text) {
-                if (text !== undefined) {
-                    result.seen = true;
-                    result.text = text;
-                }
-                deferred.resolve(result);
-           });
-//        } catch (NoAlertOpenError) {
-            //console.log("no alert");
-//        }
-        return deferred.promise;
-    }
-);
-
-wd.addPromiseChainMethod(
-    'check',
-    function () {
-        //TODO: Enable multiple assertions
-        return Q.all([this.checkAlert()]);
-    }
-);
-
-var browser = wd.promiseChainRemote();
-
-var openHTML = function (filename) {
     var deferred = Q.defer();
-    browser
-        .init({
-            browserName: 'firefox'
-        })
-        .get("http://localhost:8000/" + filename)
-        .check()
-        .then(function (results) {
-            deferred.resolve(results[0]);
-        })
-        .fin(function () {
-            return browser.quit();
-        })
-        .done();
+    var driver;
+
+    var result = {
+        type: "alert",
+        seen: "",
+        data: "",
+        error: ""
+    }
+
+    var seenAlert = function (alert) {
+        var d = Q.defer();
+        alert.getText().then(function (text) {
+            console.log("seen alert msg: " + text);
+            result.seen = true;
+            result.data = text;
+            d.resolve(result);
+        });
+        return d.promise;
+    };
+
+    var noAlert = function (e) {
+        console.log("no alert caught");
+        result.seen = false;
+        return result;
+    };
+
+    var cleanUpAndResolve = function (result) {
+        console.log("quitting ....");
+        driver.quit();
+        deferred.resolve(result);
+    };
+
+    driver = new webdriver.Builder().
+        withCapabilities(webdriver.Capabilities.chrome()).
+        build();  
+
+    driver.get("http://localhost:8000/" + fpath)
+    setTimeout(function () {
+        var alert = driver.switchTo().alert();
+        setTimeout(function () {
+            alert
+             .then(seenAlert, noAlert)
+             .then(cleanUpAndResolve);
+        }, 500);
+    }, 500);
+
     return deferred.promise;
-};
+}
+
+function main () {
+    var tests = ["gooda.html", "bad.html", "bad.html", "good.html"];
+    var testPromises = tests.map(browser);
+    return Q.all(testPromises);
+}
+
+main()
+ .then(function (results) {
+    console.log("Receving results...");
+    console.log(results);
+  })
 
 /*
-function testMain(tests) {
-    Q.try(function (tests) {
-            var A = [];
-            for (var i = 0; i < tests.length; i++) {
-                A.push(openHTML(tests[i]));
-            }
-            return Q.all(A);
-        }(tests))
-        .then(function (results) {
-            console.log(results.length);
-            console.log(results[0]);
-            console.log(results[1]);
-        })
-        .catch(function (error) {
-            console.log("exception caught");
-            console.log(error);
-        });;
-}
+browser("bad.html")
+ .then(function (result) {
+    console.log("Receving result...");
+    console.log(result);
+  })
 */
-
-function testMain(tests) {
-    var A = [];
-    for (var i = 0; i < tests.length; i++) {
-        A.push(openHTML(tests[i]));
-    }
-    return Q.all(A)
-}
-testMain(["bad.html", "output/0_0_django.html",
-    "bad.html", "bad.html"])
-    .then(function (results) {
-        console.log(results);
-    });
-exports.testMain = testMain;
