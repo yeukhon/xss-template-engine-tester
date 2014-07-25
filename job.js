0d40c84d27c394926b4a2c2f1f18461d07c3760f
Yeuk Hon Wong <yeukhon@acm.org>
2014-07-25 20:27:07 +0000

    Rewriting job.js to use kue and calling our webdriver code.

--- job.js
+++ job.js
@@ -1,25 +1,35 @@
 var kue = require('kue'),
-    jobs = kue.createQueue();
-var Q = require('q');
+    jobs = kue.createQueue(),
+    Q = require('q'),
+    browser = require('./main.js').browser;
 
-var db = {};
+jobs.process('tests', function (job, done) {
+    var files = job.data.files;
+    var promises = files.map(testHTML);
+    Q.all(promises)
+        .then(function(res) {
+            console.log("TESTS", job.id, "returning:", res);
+            return res;
+        });
+});
 
-function parentJob(id, done) {
-    var job = jobs.create('parent', {
-        type: 'PARENT',
-        id: id,
-        total: 2
+var TestsJob = function (filenames) {
+    var job = jobs.create('tests', {
+        files: filenames
     });
     job
-        .on('complete', function() {
-            console.log('Job', job.id, 'of type', job.data.type, '#', job.data.id, ' is done');
-            done();
+        .on('complete', function (res) {
+            console.log('TESTS', job.id, 'is done');
+            console.log('Result:', res);
         })
-        .on('failed', function() {
-            console.log('Job', job.id, 'of type', job.data.type, '#', job.data.id, ' has failed');
-            done();
+        .on('failed', function () {
+            console.log('TESTS', job.id, 'has failed');
         })
     job.save();
+};
+
+function testHTML(filename) {
+    return browser(filename);
 }
 
 function setJobProgress(pjob) {
@@ -28,65 +38,4 @@ function setJobProgress(pjob) {
     });
 };
 
-function childJob(cid, parentJob, done) {
-    var deferred = Q.defer();
-    var job = jobs.create('child', {
-        type: 'CHILD',
-        cid: cid
-    });
-    job
-        .on('complete', function() {
-            setJobProgress(parentJob);
-            console.log('Job', job.id, 'of type', job.data.type,
-                '#', job.data.cid, ' is done');
-            deferred.resolve({
-                done: true,
-                job: job.data,
-                success: true
-            });
-        })
-        .on('failed', function() {
-            console.log('Job', job.id, 'of type', job.data.type,
-                '#', job.data.cid, ' has failed');
-            deferred.resolve({
-                done: true,
-                job: job.data,
-                success: false
-            });
-        })
-    job.save();
-    return deferred.promise;
-}
-
-jobs.process('parent', function(job, done) {
-    /* carry out all the parent job functions here */
-    var promises = [
-        job.data.id + '-' + 1,
-        job.data.id + '-' + 2
-    ].map(
-        function(x) {
-            return childJob(x, job, done)
-        });
-
-    Q.all(promises)
-        .then(function(res) {
-            console.log('all jobs for parent ', job.data.id,
-                ' completed');
-            console.log(res);
-            done();
-        });
-
-})
-
-jobs.process('child', function(job, done) {
-    /* carry out all the child job functions here */
-    setTimeout(function() {
-        console.log('working on child job ' + job.data.cid);
-        done();
-    }, 2000);
-})
-
-parentJob(13, function() {});
-//parentJob(14, function() {});
-//parentJob(15, function() {});
-//parentJob(16, function() {});
+var t1 = TestsJob(["bad.html", "good.html"]);
