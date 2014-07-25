c9f81da73c5ead6e711e954167ffd007c5612f74
Yeuk Hon Wong <yeukhon@acm.org>
2014-07-25 20:55:30 +0000

    Now lanuchuing webdriver jobs with kue using my dumbDB.

--- job.js
+++ job.js
@@ -3,33 +3,65 @@ var kue = require('kue'),
     Q = require('q'),
     browser = require('./main.js').browser;
 
+var dumbDB = {
+    db: {},
+    push: function (data) {
+        this.db[data.rid] = data;
+    },
+    update: function (rid, data) {
+        for (key in data) {
+            this.db[rid][key] = data[key];
+        }
+    },
+    dump: function () {
+        return this.db;
+    }
+};
+
 jobs.process('tests', function (job, done) {
     var files = job.data.files;
-    var promises = files.map(testHTML);
-    Q.all(promises)
-        .then(function(res) {
-            console.log("TESTS", job.id, "returning:", res);
-            return res;
+    var job_promises = files.map(createHTMLJob);
+    Q.all(job_promises)
+     .then(function (res) {
+        console.log(res);
+        dumbDB.update(job.data.rid, {
+            result: res
         });
+        done();
+    });
 });
 
-var TestsJob = function (filenames) {
-    var job = jobs.create('tests', {
-        files: filenames
-    });
+var TestsJob = function (data) {
+    // First, create an entry in the database
+    dumbDB.push(data);
+    var job = jobs.create('tests', data);
     job
-        .on('complete', function (res) {
+        .on('complete', function () {
             console.log('TESTS', job.id, 'is done');
-            console.log('Result:', res);
+            dumbDB.update(job.data.rid, {
+                finished: true,
+                running: false,
+                success: true
+            });
         })
         .on('failed', function () {
             console.log('TESTS', job.id, 'has failed');
+            dumbDB.update(job.data.rid, {
+                finished: true,
+                running: false,
+                success: false
+            });
         })
     job.save();
 };
 
-function testHTML(filename) {
-    return browser(filename);
+function createHTMLJob(filename) {
+    var deferred = Q.defer();
+    browser(filename)
+        .then(function (res) {
+            deferred.resolve(res);
+        });
+    return deferred.promise;
 }
 
 function setJobProgress(pjob) {
@@ -38,4 +70,13 @@ function setJobProgress(pjob) {
     });
 };
 
-var t1 = TestsJob(["bad.html", "good.html"]);
+var t1 = TestsJob({
+    rid: 11111111,
+    files: ["bad.html", "good.html"]
+});
+
+setTimeout(function () {
+    var a = dumbDB.dump();
+    console.log("results: ", a["11111111"].result[0],
+        a["11111111"].result[1]);
+}, 5000);
